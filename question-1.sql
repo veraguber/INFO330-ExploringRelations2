cbae4229eb82ea49691d685a40316acf94c67ba3
VERAGUBER <VERA2@UW.EDU>
2023-04-10 13:33:25 -0700

    Completed question 1

--- question-1.sql
+++ question-1.sql
@@ -1,1 +1,2 @@
 -- Show the customer's full name, Invoice ID, Date of the invoice and billing country of customers who are from Brazil.
+SELECT c.FirstName, c.LastName, i.InvoiceID, i.InvoiceDate, i.BillingCountry FROM customers c, invoices i WHERE c.Country == 'Brazil' AND i.CustomerID == c.CustomerId;
