26719dd835cc84ada176ff3f409b55e9abde382b
VERAGUBER <VERA2@UW.EDU>
2023-04-10 17:37:28 -0700

    Completed problem 5

--- question-5.sql
+++ question-5.sql
@@ -1,2 +1,2 @@
 -- What was the most purchased track of 2013?
- SELECT t.Name, COUNT(t.Name) as 'Number of Track Purchases' FROM tracks t, invoice_items ii, invoices i WHERE ii.TrackId ==  t.TrackId AND ii.InvoiceId == i.InvoiceId and i.InvoiceDate LIKE '2013%' GROUP BY t.Name ORDER BY COUNT(t.Name) DESC LIMIT 1;
+SELECT t.Name as 'Purchased Track Name', COUNT(t.Name) as 'Number of Track Purchases' FROM tracks t, invoice_items ii, invoices i WHERE ii.TrackId ==  t.TrackId AND ii.InvoiceId == i.InvoiceId and i.InvoiceDate LIKE '2013%' GROUP BY t.Name ORDER BY COUNT(t.Name) DESC LIMIT 1;
